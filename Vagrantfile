de0efd66d95bdb84d9772d8813585d8956d6f2cd
haeyeon, hwang <haeyun@gmail.com>
2024-11-15 16:50:32 +0900

    make it better

---
+++ Vagrantfile
@@ -0,0 +1,43 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+box = "boxen/ubuntu-22.04"
+ver = "20241115.rev1.0"
+num = 2
+port = 1003
+master = "k8s-m"
+worker = "k8s-w"
+
+Vagrant.configure("2") do |config|
+  config.vm.define master do |c|
+    c.vm.box=box
+    c.vm.provider :hyperv do |v|
+      v.vmname=master
+      v.cpus=1
+      v.memory=1024
+      v.linked_clone=true
+    end
+
+    c.vm.hostname=master
+    c.vm.synced_folder ".", "/vagrant", disabled: true
+    c.vm.network "forwarded_port", guest: 22, host: "#{port}0", auto_correct: true, id: "ssh"
+    c.vm.provision "shell", path: "bootstrap.sh" 
+  end
+
+  (1..num).each do |n|
+    config.vm.define "#{worker}#{n}" do |c|
+      c.vm.box=box
+      c.vm.provider :hyperv do |v|
+        v.vmname="#{worker}#{n}"
+        v.cpus=1
+        v.memory=1024
+        v.linked_clone=true
+      end
+
+      c.vm.hostname="#{worker}#{n}"
+      c.vm.synced_folder ".", "/vagrant", disabled: true
+      c.vm.network "forwarded_port", guest: 22, host: "#{port}#{n}", auto_correct: true, id: "ssh"
+      c.vm.provision "shell", path: "bootstrap.sh" 
+    end
+  end
+end
---
+++ bootstrap.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+overlay
+br_netfilter
+EOF
+
+modprobe overlay
+modprobe br_netfilter
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sysctl --system
+apt install -y containerd
+mkdir -p /etc/containerd
+containerd config default | tee /etc/containerd/config.toml
+sed -i 's/SystemdCgroup = false/SystemdCgroup = true/g' /etc/containerd/config.toml
+systemctl restart containerd.service
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl net-tools fontconfig unzip
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.27/deb/ /" | tee /etc/apt/sources.list.d/kubernetes.list
+
+apt-get update
+apt-get install -y kubelet kubeadm kubectl
+apt-mark hold kubelet kubeadm kubectl
+
+wget https://github.com/naver/d2codingfont/releases/download/VER1.3.2/D2Coding-Ver1.3.2-20180524.zip
+unzip -d /usr/share/fonts/d2coding D2Coding-Ver1.3.2-20180524.zip
+rm D2Coding-Ver1.3.2-20180524.zip
+sudo fc-cache -f -v
